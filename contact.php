035f39dc3b25fd54eab237e120ce014c63d461dc
Nishan Dangi <89106033+nishandangi@users.noreply.github.com>
2021-08-28 00:40:33 +0400

    Add files via upload

--- contact.php
+++ contact.php
@@ -3,14 +3,11 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-
-
 $to = "nishandangi450@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message.  ;
-            
-$headers = "From: nslove450@gmail.com" . "\r\n" .
-"CC: ns.nishandangi@gmail.com";
+$subject = "Mail From ns";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: ns.nishandangi@gmai.com" . "\r\n" .
+"CC: nslove45@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
