aa0878d1136a263d53d77afaf1aae95b7d285287
Nishan Dangi <89106033+nishandangi@users.noreply.github.com>
2021-08-27 21:37:10 +0400

    Update email.php

--- email.php
+++ email.php
@@ -3,16 +3,16 @@
 $name =$_request['name'];
 $email =$_request['email'];
 $message =$_request['message'];
-$file =$_request['file'];
+
 
 //check input fields
-if (empty($name) || empty($email) || empty(message) || empty(file)     )
+if (empty($name) || empty($email) || empty($message)     )
 {
 	echo "please complete the process";
 
 }
 else{
-	mail("ns.nishandangi@gmail.com" ,"request from web" , "$message" ,"$file" );
+	mail("ns.nishandangi@gmail.com" ,"request from web" , "$message" ,"From:$name <$email>"  );
 	echo "<script type='text/javascript' >alert(' your request sent successfully, you will be guided soon through mail');
 	window.history.log(-1);
 	</script>"
