7f3f2604d5f5adc4c1b1bac4afcaf1f3cd19e88d
Nishan Dangi <89106033+nishandangi@users.noreply.github.com>
2022-04-05 23:13:21 +0400

    Add files via upload

---
+++ ddd.bat
@@ -0,0 +1,16 @@
+@echo off
+cd C:
+
+::powershell -command "invoke-webrequest https://nishandangi.com.np/hkjnsADoncnxoIDOIWSJsdQWIODSJDASQEWIOJIJSDBSJSKJNSKJ.png -outfile  "C:\Users\%username%\Desktop\fall.bmp"
+::timeout 10 /nobreak 
+reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d  "C:\Users\%username%\Desktop\fall.bmp" /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+
+timeout 1
+del /s /q "C:\Users\%username%\Desktop\fall.bmp"
+timeout 1
+del /s /q "C:\Users\%username%\Desktop\aa.bat"
+timeout 1
+del /s /q "C:\Users\%username%\Desktop\cb.bat"
+
+exit
