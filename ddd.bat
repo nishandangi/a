5af10f2ac152c371fdecf7c74fe037caa899d419
Nishan Dangi <89106033+nishandangi@users.noreply.github.com>
2024-06-11 17:23:36 +0545

    Update ddd.bat

--- ddd.bat
+++ ddd.bat
@@ -1,10 +1,1 @@
-@echo off
-On Error Resume Next
-cd C:
 
-::powershell -command "invoke-webrequest https://nishandangi.com.np/hkjnsADoncnxoIDOIWSJsdQWIODSJDASQEWIOJIJSDBSJSKJNSKJ.png -outfile  "C:\Users\%username%\Desktop\fall.bmp"
-::timeout 10 /nobreak 
-reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d  "C:\Users\%username%\Desktop\fall.bmp" /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
-
-exit
